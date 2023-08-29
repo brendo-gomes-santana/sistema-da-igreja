42e0e4a0e8d2eb69e35c769315eee85967964bc4
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-08-29 16:44:49 -0300

    fazendo outro teste para fazer funcionar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 import app from './app';
 const server = http.createServer(app);
+
 server.listen(process.env.PORTA, () => {
     console.log(`SISTEMA DA FUNCIONANDO NA PORTA ${process.env.PORTA}`)
 })
