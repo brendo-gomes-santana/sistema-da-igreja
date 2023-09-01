1154274a2903877d921a2217c6bb21c0c4aa87ff
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-09-01 17:16:45 -0300

    tentando ajetar o sistema

--- backend/build/server.js
+++ backend/build/server.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 const app_1 = __importDefault(require("./app"));
+const http_1 = __importDefault(require("http"));
 if (process.env.NODE_ENV !== "production") {
     (0, dotenv_1.config)();
 }
 const PORT = process.env.PORT || 3003;
-app_1.default.listen(PORT, () => {
+const server = http_1.default.createServer(app_1.default);
+server.listen(PORT, () => {
     console.log(`SISTEMA DA FUNCIONANDO NA PORTA ${PORT}`);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import { config } from 'dotenv';
 import app from './app';
-
+import http from 'http'
+import yearsToMonths from 'date-fns/yearsToMonths';
 if (process.env.NODE_ENV !== "production") {
     config();
   }
 
 const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
     console.log(`SISTEMA DA FUNCIONANDO NA PORTA ${PORT}`)
 })
