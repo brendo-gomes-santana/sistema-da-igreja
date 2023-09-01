dcdda62529becba331dff788ee6d9373fb088116
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-09-01 16:08:30 -0300

    colocando tsup para compilar direito

---
+++ backend/tsup.config.js
@@ -0,0 +1,12 @@
+// tsup.config.js
+
+module.exports = {
+    // ... outras configurações ...
+  
+    // Inclua os arquivos de declaração na compilação
+    dts: true,
+  
+    // Configuração de saída
+    outDir: 'dist',
+  };
+  
